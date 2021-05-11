94fcc9c87f6278a661ad80e1e3d8b6a9608dc8b3
Aashish Bansal <aashish22bansal@gmail.com>
2021-05-11 16:48:17 +0530

    Day 3: Intro to Conditional Statements

---
+++ 30 Days of Code/Day_3_Intro_to_Conditional_Statements.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Day_3_Intro_to_Conditional_Statements {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        if(N%2!=0){
+            System.out.println("Weird");
+        }
+        else if(N%2==0){
+            if(N>=2 && N<=5){
+                System.out.println("Not Weird");
+            }
+            if(N>=6 && N<=20){
+                System.out.println("Weird");
+            }
+            if(N>20){
+                System.out.println("Not Weird");
+            }
+        }
+        bufferedReader.close();
+    }
+}

1990883a7e6706ab8234b00ec41b179dd3c6e64d
aashish22bansal <aashish22bansal@gmail.com>
2021-08-05 09:40:19 +0530

    Java Sort

--- Java/Java_Sort.java
+++ Java/Java_Sort.java
@@ -25,13 +25,39 @@ class Student{
 
 class SortById implements Comparator<Student>{
 	public int compare(Student S1, Student S2){
-		return S1.id - S2.id;
+		return S1.getId() - S2.getId();
+	}
+}
+
+class SortByName implements Comparator<Student>{
+	public int compare(Student S1, Student S2){
+		return S1.getFname().compareTo(S2.getFname());
+	}
+}
+
+class SortByCGPA implements Comparator<Student>{
+	public int compare(Student S1, Student S2){
+		if(Double.compare(S1.getCgpa(),S2.getCgpa())>=0){
+			return -1;
+		}
+		else{
+			return 1;
+		}
 	}
 }
 
 //Complete the code
 public class Java_Sort
 {
+	private static boolean isStringLowerCase(String str){
+		char[] charArray = str.toCharArray();
+		for(int i=0;i<charArray.length;i++){
+			if(!Character.isLowerCase(charArray[i])){
+				return false;
+			}
+		}
+		return true;
+	}
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
         System.out.println("Enter the number of students: ");
@@ -51,40 +77,46 @@ public class Java_Sort
 			int id = in.nextInt();
             if((id<0)||(id>100000)){
                 while((id<0)||(id>100000)){
-                    System.out.println("Enter the valid Student ID for student "+(i+1)+": "); 
+                    System.out.println("Enter the valid ID for student "+(i+1)+": "); 
                     id = in.nextInt();
                 }
             }
+
             System.out.println("Enter the Student Name for student "+(i+1)+": "); 
 			String fname = in.next();
+			if(((fname.length()<5)||(fname.length()>30))||(isStringLowerCase(fname))){
+				while(((fname.length()<5)||(fname.length()>30))||(isStringLowerCase(fname))){
+					System.out.println("Enter the valid Name for the student "+(i+1)+": "); 
+					fname = in.next();
+				}
+			}
+
             System.out.println("Enter the Student CGPA for student "+(i+1)+": "); 
 			double cgpa = in.nextDouble();
-            if((cgpa<0.0)||(cgpa>4.0)){
-                while((cgpa<0.0)||(cgpa>4.0)){
+			String text = Double.toString(Math.abs(cgpa));
+			int integerPlaces = text.indexOf('.');
+			int decimalPlaces = text.length()-integerPlaces-1;
+            if(((cgpa<0.0)||(cgpa>4.0))||((decimalPlaces>2)||(decimalPlaces<0))){
+                while(((cgpa<0.0)||(cgpa>4.0))||((decimalPlaces>2)||(decimalPlaces<0))){
                     System.out.println("Enter the valid Student CGPA for student "+(i+1)+": "); 
                     cgpa = in.nextDouble();
                 }
             }
+
 			// Creating an object for the student
             System.out.println("\nCreating an object for the student: ");
 			Student st = new Student(id, fname, cgpa);
+
+			// Adding the object to the list
 			studentList.add(st);
 			System.out.println("\nThe student has been added to the list!");
 			testCases--;
             i++;
 		}
 
-		// Creating a custom comparator
-		// Comparator<Student> compareById = new Comparator<Student>(){
-		// 	@Override
-		// 	public int compare(Student S1, Student S2){
-		// 		return S1.getFname().compareTo(S2.getFname());
-		// 	}
-		// };
         // Sorting a list in java
-		Collections.sort(studentList, new SortById());
-		// Comparator<Student> compareById = (Student S1, Student S2)->S1.getId().compareTo(S2.getId());
-		// Comparator<Student> compareById = (Student S1, Student S2)->S1.getFname().compareTo(S2.getFname());
+		Collections.sort(studentList, new SortByCGPA());
+		
 		// Printing a sorted list
 		System.out.println("The names of the students in the sorted order are: ");
       	for(Student st: studentList){

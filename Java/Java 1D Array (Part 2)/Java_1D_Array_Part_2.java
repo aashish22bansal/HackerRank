6927ff3cf5025c5d6694e67e63864bc2ecbe83ff
aashish22bansal <aashish22bansal@gmail.com>
2021-08-17 12:07:16 +0530

    Question solved and completed

--- Java/Java 1D Array (Part 2)/Java_1D_Array_Part_2.java
+++ Java/Java 1D Array (Part 2)/Java_1D_Array_Part_2.java
@@ -3,59 +3,22 @@ import java.util.*;
 public class Java_1D_Array_Part_2 {
 
     public static boolean canWin(int leap, int[] game) {
-        // Return true if you can win the game; otherwise, return false.
-        int flag=0;
-        for(int i=0;i<game.length;){
-            if(((i+leap)>=(game.length))||(i==(game.length)))
-            { // Checking if we can directly reach the end
-                flag=1;
-                break;
-            }
-            else
-            { // Checking other cases when we cannot reach the end directly
-                if(game[i+leap]==0)
-                { // Jump Comdition
-                    i=i+leap;
-                }
-                else if(game[i+leap]==1)
-                { // When jump is not possible
-                    if(game[i+1]==0)
-                    { // Taking a step forward
-                        i++;
-                    }
-                    else if(game[i+1]==1)
-                    { // if it is not possible to take a step forward
-                        if((i-1)<0){
-                            flag = 0;
-                            break;
-                        }
-                        else{
-                            if(game[i-1]==0)
-                            { // Take a step back
-                                if(i==1)
-                                { // Checking if we back on start
-                                    flag=0;
-                                    break;
-                                }
-                                else{
-                                    i--;
-                                }
-                            }
-                            else if(game[i-1]==1){
-                                flag=0;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        if(flag==1){
-            return true;
-        }
-        else{
+        if(game==null){
             return false;
         }
+        return isSolvable(leap,game,0);
+    }
+
+    private static boolean isSolvable(int leap, int[] game, int i){
+        if(i>=game.length){
+            return true;
+        }
+        else if(i<0 || game[i]==1){
+            return false;
+        }
+        game[i] = 1;
+
+        return isSolvable(leap, game, i+leap) || isSolvable(leap, game, i+1) || isSolvable(leap, game, i-1);
     }
 
     public static void main(String[] args) {

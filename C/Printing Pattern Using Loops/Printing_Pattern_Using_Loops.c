77d01e7b2c0def40312416d1b106d449f18ba991
aashish22bansal <aashish22bansal@gmail.com>
2021-08-20 00:33:44 +0530

    Printing Pattern Using IC

--- C/Printing Pattern Using Loops/Printing_Pattern_Using_Loops.c
+++ C/Printing Pattern Using Loops/Printing_Pattern_Using_Loops.c
@@ -5,9 +5,12 @@
 
 int main() 
 {
-
     int n;
     scanf("%d", &n);
+    int center_print_controller=0;
   	// Complete the code to print the pattern.
+    for(int i=1;                               i<((2*center_print_controller)-1); i++ ){}
+    for(int j=center_print_controller;         j==center_print_controller       ;     ){}
+    for(int k=((2*center_print_controller)-1); k>0                              ; k-- ){}
     return 0;
 }

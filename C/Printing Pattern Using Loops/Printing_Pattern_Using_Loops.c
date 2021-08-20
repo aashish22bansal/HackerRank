5a81b270c26b8b836a35e74420e089495bf830fc
aashish22bansal <aashish22bansal@gmail.com>
2021-08-21 00:21:20 +0530

    Printing Pattern Using C
    HackerRank

--- C/Printing Pattern Using Loops/Printing_Pattern_Using_Loops.c
+++ C/Printing Pattern Using Loops/Printing_Pattern_Using_Loops.c
@@ -9,8 +9,8 @@ int main()
     scanf("%d", &n);
     int center_print_controller=0;
   	// Complete the code to print the pattern.
-    for(int i=1;                               i<((2*center_print_controller)-1); i++ ){}
-    for(int j=center_print_controller;         j==center_print_controller       ;     ){}
-    for(int k=((2*center_print_controller)-1); k>0                              ; k-- ){}
+    // for(int i=1;                               i<((2*center_print_controller)-1); i++ ){}
+    // for(int j=center_print_controller;         j==center_print_controller       ;     ){}
+    // for(int k=((2*center_print_controller)-1); k>0                              ; k-- ){}
     return 0;
 }
